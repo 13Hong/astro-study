f83474722052867fa7e56615a8104cdce644a426
Hong <571507098@qq.com>
2024-07-09 21:06:23 +0800

    completely

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site:"https://hiroblog.netlify.app/"
+  site: "https://hiroblog.netlify.app/",
+  integrations: [preact()]
 });
